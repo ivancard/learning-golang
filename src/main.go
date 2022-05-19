d531a6e6e27a9d7f9074bbede85969909d514026
ivancard <ivcardenas@alumno.unlam.edu.ar>
2022-05-19 17:22:21 -0300

    zero values

--- src/main.go
+++ src/main.go
@@ -16,5 +16,31 @@ func main()  {
   var area int = base * height
 
   fmt.Println("+==============+")
-  fmt.Println("\nArea:",area)
+  fmt.Println("Area:",area)
+  
+  fmt.Println("+==============+")
+
+  // Zero values - default values
+
+  var a int
+  var b float64
+  var c string
+  var d bool
+
+  fmt.Println("int:", a,
+              "\nfloat64:", b, 
+              "\nstring:", c, 
+              "\nbool:", d)
+  // Printing the types of default values
+  fmt.Println("--types--")
+  fmt.Printf("int a is: %T", a)
+  fmt.Printf("\nfloat64 b is: %T", b)
+  fmt.Printf("\nstring c is: %T", c)
+  fmt.Printf("\nbool d is: %T\n", d)
+
+  /*
+    Conclusion:
+      The default values never change the type
+      of the declarated variable.
+  */
 }
