10ec89f1004957fd8e444c2f5c1159b7c4225952
ivancard <ivcardenas@alumno.unlam.edu.ar>
2022-05-19 16:50:54 -0300

    learning variables declarations

--- src/main.go
+++ src/main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main()  {
+  const pi float64 = 3.14
+  const pi2 = 3.1415
   fmt.Println("Hello World")
+  fmt.Println("Pi: ", pi)
+  fmt.Println("Pi2: ",pi)
+
+  // Declaration of interger numbers.
+
+  base := 12
+  var height int = 14
+  var area int = base * height
+
+  fmt.Println("+==============+")
+  fmt.Println("Area:",area)
 }
