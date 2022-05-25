81e1d39553df5b12c71e216d7b70434c94f70016
ivancard <ivcardenas@alumno.unlam.edu.ar>
2022-05-24 23:18:55 -0300

    go routines

---
+++ src/goroutines.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func say(text string, wg *sync.WaitGroup) {
+  defer wg.Done()
+	fmt.Println(text) 
+}
+
+func main() {
+
+  var wg sync.WaitGroup
+
+  // fmt.Println(wg)
+
+	fmt.Println("Hello")
+	wg.Add(1)
+	// fmt.Println(wg)
+	go say("World", &wg)
+
+  wg.Wait()
+
+}
