fa4c9559007503ca52d5b6ba1e8cbe768cf0269a
ivancard <ivcardenas@alumno.unlam.edu.ar>
2022-05-24 21:41:03 -0300

    pointers

---
+++ src/pointers.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
+
+func (myPc pc)ping()  {
+  fmt.Println(myPc.brand, "Pong")
+  
+}
+
+func (myPc *pc) duplicateRam()  {
+  myPc.ram = myPc.ram * 2
+}
+
+func main() {
+	a := 50
+	b := &a
+
+	fmt.Println("a is: ", a)                                    //50
+	fmt.Println("b is a pointer to a, and the position is:", b) //hexcode
+	fmt.Printf("b is a type: %T\n", b)                          //*int
+	fmt.Println("the content of pointer b is:", *b)             //50
+
+	*b = 51
+	fmt.Println("the content of a modified by his pointer is:", a)
+fmt.Println("=====================")
+  myPc := pc{ram: 16, disk: 5, brand: "IBM"}
+  // fmt.Println(myPc)
+  myPc.ping()
+  fmt.Println(myPc)
+  myPc.duplicateRam()
+  fmt.Println(myPc)
+
+}
